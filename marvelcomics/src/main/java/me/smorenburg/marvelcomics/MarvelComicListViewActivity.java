f26312bb9cd975c65ba3664fb81efee29821d96b
Ivor Smorenburg Aguado <ivorsmorenburgaguado@gmail.com>
2016-04-08 19:07:37 +0100

    First Init Commit Project

---
+++ marvelcomics/src/main/java/me/smorenburg/marvelcomics/MarvelComicListViewActivity.java
@@ -0,0 +1,13 @@
+package me.smorenburg.marvelcomics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarvelComicListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marvel_comic_list_view);
+    }
+}
---
+++ marvelcomics/src/main/java/me/smorenburg/marvelcomics/base/MArvelApplicationBase.java
@@ -0,0 +1,7 @@
+package me.smorenburg.marvelcomics.base;
+
+/**
+ * Created by Ivor Smorenburg Aguado on 08/04/2016.
+ */
+public class MArvelApplicationBase {
+}
