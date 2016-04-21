b5935c06d186a52aac565b23b6fd6089636682b4
Clemente Tort Barbero <clementetb@gmail.com>
2016-04-21 01:57:30 +0100

    Fix crash onrotate

--- Marvellous/app/src/main/java/com/streetbees/clementetort/marvellous/ui/ComicListActivity.java
+++ Marvellous/app/src/main/java/com/streetbees/clementetort/marvellous/ui/ComicListActivity.java
@@ -84,8 +84,8 @@ public class ComicListActivity extends RealmActivity implements ComicAdapter.Com
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
         files.removeChangeListener(this);
+        super.onDestroy();
     }
 
     private void listenDropboxFiles() {
